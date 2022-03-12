13958dd353c7c73ce69e2e5a7fab2a7661c583e3
jnskender <jnskender@gmail.com>
2022-03-12 12:14:33 -0600

    add dev refresh to cron

--- src/createCron.js
+++ src/createCron.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env zx
 import 'zx/globals'
 
-const nodePath = await $`which node`
-let refresherScriptPath = await path.resolve("index.js");
-await $`echo ${nodePath}`
+const refresherScriptPath = await path.resolve("index.js");
+const devRefresher = await path.resolve("developerLogin.js");
 
 //have to trim standard output otherwise /n will be appended to nodePath
-const cronCmd = nodePath.toString().trim() + " " + refresherScriptPath;
-const cronJob = `0 */12 * * * ${cronCmd}`; //run command every 12 hours
+createCron(refresherScriptPath, "0 */12 * * *");
+createCron(devRefresher, "0 */6 * * *");
+
+await $`crontab -l`
+
+async function createCron (script, schedule) {
+  const nodePath = await $`which node`;
+  const cronCmd = nodePath.toString().trim() + " " + script;
+  const cronJob = `${schedule} ${cronCmd}`; //run command every 12 hours
 //add it to crontab, with no duplication
-await $`( crontab -l | grep -v -F ${cronCmd} ; echo ${cronJob} ) | crontab -`;
+  await $`( crontab -l | grep -v -F ${cronCmd} ; echo ${cronJob} ) | crontab -`;
+}
